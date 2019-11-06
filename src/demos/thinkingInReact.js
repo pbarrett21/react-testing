ed48cc11e844b8b83fb2b291d586c6b9adc70307
Paul Barrett and Tyler Dimon <pair+Paul.Barrett+John.Dimon@CVSHealth.com>
2019-11-06 15:52:36 -0600

    social card idea

--- src/demos/thinkingInReact.js
+++ src/demos/thinkingInReact.js
@@ -125,9 +125,11 @@ const PRODUCTS = [
     {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
     {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
     {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+    {category: "Sporting Goods", price: "$14.99", stocked: false, name: "Soccer ball"},
     {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
     {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-    {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+    {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"},
+    {category: "Electronics", price: "$249.99", stocked: true, name: "Airpods Pro"}
 ];
 
 export default FilterableProductTable;
