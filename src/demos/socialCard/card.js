eae060a4418ed0047a24c64f277af3d2dbe6dfe4
Paul Barrett and Tyler Dimon <pair+Paul.Barrett+John.Dimon@CVSHealth.com>
2019-11-07 14:46:59 -0600

    social card json

---
+++ src/demos/socialCard/card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Card extends React.Component {
+
+}
+
+POSTS = [
+    {user: 'Fastturtle4', header: 'First post', text: 'Testing the first post', image: null},
+    {user: 'Paul.barrett21', header: 'Second post', text: 'second', image: null},
+    {user: 'Yeetbro', header: 'YEET', text: 'y33t', image: null},
+    {user: 'Stonks', header: 'ACB to the MOON', text: 'Invest in ACB and get rich!', image: null}
+];
+
+export default Card;
