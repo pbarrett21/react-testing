076541d8d50f795f26d96f51a9dfd6f56d7b1ea9
Paul Barrett and Tyler Dimon <pair+Paul.Barrett+John.Dimon@CVSHealth.com>
2019-11-11 12:45:18 -0600

    links

--- src/demos/socialCard/card.js
+++ src/demos/socialCard/card.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 import './styles.css';
 import turt from './../../images/turtpic.jpg';
 import banana from './../../images/banana.jpg';
@@ -8,15 +14,28 @@ import acb from './../../images/acb.jpg';
 class Card extends React.Component {
     render() {
         const post = this.props.post;
+        const link = "/" + post.user;
         return (
-            <div className="container">
-                <b>{post.user} - {post.header}</b>
-                <img src={post.image} alt={post.header} />
-                <p>{post.text}</p>
-                <hr></hr>
-            </div>
+            <Router>
+                <div className="container">
+                    <b><Link to={link}>{post.user}</Link> - {post.header}</b>
+                    <img src={post.image} alt={post.header} />
+                    <p>{post.text}</p>
+                    <hr></hr>
+                </div>
+
+                <Switch>
+                    <Route path={link}>
+                        <Test />
+                    </Route>
+                </Switch>
+            </Router>
         );
     }
+}
+
+function Test() {
+    return <h2>Test</h2>;
 }
 
 class Feed extends React.Component {
@@ -24,7 +43,7 @@ class Feed extends React.Component {
         const posts = [];
         POSTS.forEach(post => {
             posts.push(
-                <Card key={post.name} post={post} />
+                <Card post={post} key={post.text} />
             );
         });
         return (
@@ -36,10 +55,10 @@ class Feed extends React.Component {
 }
 
 const POSTS = [
-    {user: 'Fastturtle4', header: 'First post', text: 'Testing the first post caption', image: turt},
-    {user: 'Paul.barrett21', header: 'Second post', text: 'second caption', image: banana},
-    {user: 'Yeetbro', header: 'YEET', text: 'y33t', image: yeet},
-    {user: 'Stonks', header: 'ACB to the MOON', text: 'Invest in ACB and get rich!', image: acb}
+    { user: 'Fastturtle4', header: 'First post', text: 'Testing the first post caption', image: turt },
+    { user: 'Paul.barrett21', header: 'Second post', text: 'second caption', image: banana },
+    { user: 'Yeetbro', header: 'YEET', text: 'y33t', image: yeet },
+    { user: 'Stonks', header: 'ACB to the MOON', text: 'Invest in ACB and get rich!', image: acb }
 ];
 
 export default Feed;
