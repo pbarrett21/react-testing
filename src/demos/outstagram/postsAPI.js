55a978c452530d193ddf602f9621f3414bd99854
Paul Barrett and Tyler Dimon <pair+Paul.Barrett+John.Dimon@CVSHealth.com>
2019-11-19 15:59:54 -0600

    change postAPI

--- src/demos/outstagram/postsAPI.js
+++ src/demos/outstagram/postsAPI.js
@@ -8,14 +8,54 @@ import image3 from './../../images/image3.jpeg';
 import image4 from './../../images/image4.jpeg';
 
 const POSTS = [
-    { user: 'User1', header: 'User1 post header', text: 'This is the caption for image 1', image: image1 },
-    { user: 'User2', header: 'User2 post header', text: 'Caption for image 2', image: image2 },
-    { user: 'User3', header: 'User3 post header', text: 'Yet another caption', image: image3 },
-    { user: 'User4', header: 'User4 post header', text: 'This is an extra long caption for image 4 to text caption wrapping', image: image4 },
-    { user: 'User5', header: 'User5 post header', text: 'Caption caption', image: turt },
-    { user: 'User6', header: 'User6 post header', text: 'User6 caption', image: banana },
-    { user: 'User7', header: 'User7 post header', text: 'User7 caption', image: yeet },
-    { user: 'User8', header: 'User8 post header', text: 'User8 caption', image: acb }
+    {
+        user: 'User1', header: 'User1 post header', text: 'This is the caption for image 1', image: image1,
+        profile: {
+            posts: 12, followers: 103, following: 62, bio: 'Simple bio for user1'
+        }
+    },
+    {
+        user: 'User2', header: 'User2 post header', text: 'Caption for image 2', image: image2,
+        profile: {
+            posts: 3, followers: 14, following: 23, bio: 'This is a bio too.'
+        }
+    },
+    {
+        user: 'User3', header: 'User3 post header', text: 'Yet another caption', image: image3,
+        profile: {
+            posts: 122, followers: 1033, following: 231, bio: 'Another bio'
+        }
+    },
+    {
+        user: 'User4', header: 'User4 post header', text: 'This is an extra long caption for image 4 to text caption wrapping', image: image4,
+        profile: {
+            posts: 12, followers: 103, following: 62, bio: 'Simple bio for user1'
+        }
+    },
+    {
+        user: 'User5', header: 'User5 post header', text: 'Caption caption', image: turt,
+        profile: {
+            posts: 12, followers: 103, following: 62, bio: 'Simple bio for user1'
+        }
+    },
+    {
+        user: 'User6', header: 'User6 post header', text: 'User6 caption', image: banana,
+        profile: {
+            posts: 12, followers: 103, following: 62, bio: 'Simple bio for user1'
+        }
+    },
+    {
+        user: 'User7', header: 'User7 post header', text: 'User7 caption', image: yeet,
+        profile: {
+            posts: 12, followers: 103, following: 62, bio: 'Simple bio for user1'
+        }
+    },
+    {
+        user: 'User8', header: 'User8 post header', text: 'User8 caption', image: acb,
+        profile: {
+            posts: 12, followers: 103, following: 62, bio: 'Simple bio for user1'
+        }
+    }
 ];
 
 export default POSTS;
