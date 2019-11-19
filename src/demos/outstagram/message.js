7eed8396edf6163544c6f467402c344ac5851fbd
Paul Barrett and Tyler Dimon <pair+Paul.Barrett+John.Dimon@CVSHealth.com>
2019-11-19 15:53:07 -0600

    messages

--- src/demos/outstagram/message.js
+++ src/demos/outstagram/message.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Message = (props) => {
     return (
         <div className="post-container">
-            {props.info.user}
+            <b>{props.info.user}</b>
+            <div className="recent-message">
+                <h6>{props.info.mostRecentMessage}</h6>
+            </div>
         </div>
     );
 }
